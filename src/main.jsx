d9ca8a8be91e4ce4aa6f256cf8c8b948cae8930e
Sarthak-Sama <sarthak.saklani04@gmail.com>
2024-08-19 15:58:18 +0530

    Fixed an import Error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Context from "./context/context.jsx";
+import Context from "./context/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Context>
